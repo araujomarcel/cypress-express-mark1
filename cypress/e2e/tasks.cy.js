a506320e2f12827c8ef1992b446dc21b22293944
araujomarcel <araujomarcel89@gmail.com>
2025-04-28 23:44:17 -0300

    insomnia

--- cypress/e2e/tasks.cy.js
+++ cypress/e2e/tasks.cy.js
@@ -2,6 +2,16 @@
 
 describe('tarefas', ()=> {
     it('deve cadastrar uma nova tarefa', ()=>{
+
+        cy.request({
+           url: 'http://localhost:3333/helper/tasks',
+           method: 'DELETE',
+           body: {name: "The Clone Wars"} 
+
+        }).then(Response => {
+            expect(Response.status).to.eq(204)
+        })
+
         cy.visit('http://localhost:3000');
         cy.get('input[placeholder="Add a new Task"]')
             .type('The Clone Wars');
