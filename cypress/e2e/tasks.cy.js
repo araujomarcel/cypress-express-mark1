5e0976fcad1cf905c4657732758caa2e5512846f
araujomarcel <araujomarcel89@gmail.com>
2025-04-29 00:15:08 -0300

    var

--- cypress/e2e/tasks.cy.js
+++ cypress/e2e/tasks.cy.js
@@ -1,12 +1,13 @@
 /// <reference types="cypress"/>
 
 describe('tarefas', ()=> {
+    const taskName = 'The clone Wars'
     it('deve cadastrar uma nova tarefa', ()=>{
 
         cy.request({
            url: 'http://localhost:3333/helper/tasks',
            method: 'DELETE',
-           body: {name: "The Clone Wars"} 
+           body: {name: 'taskName'} 
 
         }).then(Response => {
             expect(Response.status).to.eq(204)
@@ -14,12 +15,12 @@ describe('tarefas', ()=> {
 
         cy.visit('http://localhost:3000');
         cy.get('input[placeholder="Add a new Task"]')
-            .type('The Clone Wars');
+            .type('taskName');
         cy.contains('button', 'Create').click();
 
         cy.get('main div p')
             .should('be.visible')
-            .should('have.text', 'The Clone Wars')
+            .should('have.text', 'taskName')
     });
     it('não deve permitir tarefas duplicadas', ()=>{
         cy.visit('http://localhost:3000');
